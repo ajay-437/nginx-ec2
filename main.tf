88b5d4267a854225c137e9b80e3ce4d90baa57cb
ajay-437 <159330699+ajay-437@users.noreply.github.com>
2024-02-07 21:20:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,21 @@
- provider "aws" {
+#
+terraform{
+required providers{
+aws= {
+source = "hashicorp/aws"
+version = "~> 4.0"
+}
+}
+}
+provider "aws" {
      region = "ca-central-1"
      profile = "terraform-user"
-     access_key = "AKIAYS2NVRSRPUD5FP5T"
-	   secret_key = "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"
-    
-    
-  
-}
+    #access_key = "AKIAYS2NVRSRPUD5FP5T"
+	  #secret_key = "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"
+    }
 resource "aws_instance" "nginx-ins" {
   ami           = "ami-0a2e7efb4257c0907"  
-  key_name   = "key-pair1"
+  
   instance_type = "t2.micro"  
   subnet_id = aws_subnet.public-subnet.id
  vpc_security_group_ids = [ aws_security_group.aws-sg.id ]
