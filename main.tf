886b6c76548cb36521e687cac16f9d0724b66c30
ajay-437 <159330699+ajay-437@users.noreply.github.com>
2024-02-08 16:26:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,25 +2,16 @@
 
 provider "aws" {
      region = "ca-central-1"
-     profile = "terraform-user"
-  
+    
     }
-resource "aws_instance" "nginx-ins" {
+resource "aws_instance" "foo" {
   ami           = "ami-0a2e7efb4257c0907"  
   
   instance_type = "t2.micro"  
-  subnet_id = aws_subnet.public-subnet.id
- vpc_security_group_ids = [ aws_security_group.aws-sg.id ]
+  tags = {
+      Name = "TF-Instance"   
 
-user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update
-              sudo apt-get install -y nginx
-              EOF  
-      
-      
-      
-    }
+}
   
 
 
