58e9feb84a3b4ac569bb141e8a9c907b6904b410
ajay-437 <159330699+ajay-437@users.noreply.github.com>
2024-02-08 19:48:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@
 
 provider "aws" {
      region = "ca-central-1"
-
+ AWS_ACCESS_KEY_ID     = credentials('aws_access_key_id')
+        AWS_SECRET_ACCESS_KEY = credentials('aws_secret_access_key')
     
     }
 resource "aws_instance" "foo" {
