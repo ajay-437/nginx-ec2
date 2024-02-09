d7df40a9ab16a215beed7ae32409b6375d6401b9
ajay-437 <159330699+ajay-437@users.noreply.github.com>
2024-02-09 14:55:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,15 @@
-
-
 provider "aws" {
      region = "ca-central-1"
- profile =  "jenkins-user"
-    
-    }
-resource "aws_instance" "foo" {
-  ami           = "ami-0a2e7efb4257c0907"  
+        
   
-  instance_type = "t2.micro"  
-  tags = {
-      Name = "TF-Instance"   
-
 }
+resource "aws_instance" "foo" {
+  ami           = "ami-0a2e7efb4257c0907"
+  key_name   = "aws-key"
+
+   instance_type = "t2.micro"
+ 
+ tags = {
+    Name = "test1"
+  }
 }
-
-
